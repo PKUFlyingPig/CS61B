7cd0f2cb4d8a03656f8516a18b589cbb85fe2da1
Josh Hug <hug@cs.berkeley.edu>
2018-01-15 18:52:04 -0800

    Added lab1 skeleton file

---
+++ lab1/LeapYear.java
@@ -0,0 +1,33 @@
+/** Class that determines whether or not a year is a leap year.
+ *  @author YOUR NAME HERE
+ */
+public class LeapYear {
+
+    /** Calls isLeapYear to print correct statement.
+     *  @param  year to be analyzed
+     */
+    private static void checkLeapYear(int year) {
+        if (isLeapYear(year)) {
+            System.out.printf("%d is a leap year.\n", year);
+        } else {
+            System.out.printf("%d is not a leap year.\n", year);
+        }
+    }
+
+    /** Must be provided an integer as a command line argument ARGS. */
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Please enter command line arguments.");
+            System.out.println("e.g. java Year 2000");
+        }
+        for (int i = 0; i < args.length; i++) {
+            try {
+                int year = Integer.parseInt(args[i]);
+                checkLeapYear(year);
+            } catch (NumberFormatException e) {
+                System.out.printf("%s is not a valid number.\n", args[i]);
+            }
+        }
+    }
+}
+
