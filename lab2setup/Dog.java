e95825bc69756b69332ee0a5d23976d3bcb9ed80
Josh Hug <josh@joshh.ug>
2018-01-21 22:50:26 -0800

    lab2setup: added files needed by students to complete lab2setup

---
+++ lab2setup/Dog.java
@@ -0,0 +1,15 @@
+public class Dog {
+    private int size;
+
+    public Dog(int s) {
+        size = s;
+    }
+
+    /** Makes a noise. */
+    public String noise() {
+        if (size < 10) {
+            return "yip";
+        } 
+        return "bark";
+    }
+}
---
+++ lab2setup/DogTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class DogTest {    
+    @Test
+    public void testSmall() {
+        Dog d = new Dog(3);
+        assertEquals("yip", d.noise());
+    }
+
+    @Test
+    public void testLarge() {
+        Dog d = new Dog(20);
+        assertEquals("bark", d.noise());
+    }
+}
