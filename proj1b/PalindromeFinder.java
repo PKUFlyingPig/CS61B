500c6d5beb39e2eb76f50ee1997fefab40df4a84
Josh Hug <josh@joshh.ug>
2018-02-04 00:46:29 -0800

    beta version of project1b skeleton files. these may change, proceed at your own peril

---
+++ proj1b/PalindromeFinder.java
@@ -0,0 +1,15 @@
+/** This class outputs all palindromes in the words file in the current directory. */
+/*public class PalindromeFinder {
+    public static void main(String[] args) {
+        int minLength = 4;
+        In in = new In("words.txt");
+        Palindrome palindrome = new Palindrome();
+
+        while (!in.isEmpty()) {
+            String word = in.readString();
+            if (word.length() >= minLength && palindrome.isPalindrome(word)) {
+                System.out.println(word);
+            }
+        }
+    }
+} Uncomment this class once you've written isPalindrome. */
---
+++ proj1b/TestPalindrome.java
@@ -0,0 +1,18 @@
+/*import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestPalindrome {
+    // You must use this palindrome, and not instantiate
+    // new Palindromes, or the autograder might be upset.
+    static Palindrome palindrome = new Palindrome();
+
+    @Test
+    public void testWordToDeque() {
+        Deque d = palindrome.wordToDeque("persiflage");
+        String actual = "";
+        for (int i = 0; i < "persiflage".length(); i++) {
+            actual += d.removeFirst();
+        }
+        assertEquals("persiflage", actual);
+    }
+}     Uncomment this class once you've created your Palindrome class. */
