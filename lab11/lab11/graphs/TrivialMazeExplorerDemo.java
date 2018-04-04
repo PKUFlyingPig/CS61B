62033e77c794aa23f32ca034f1de68dbe507b4a7
Wayne Li <wli2@berkeley.edu>
2018-04-04 14:30:07 -0700

    lab11: Update trivial

--- lab11/lab11/graphs/TrivialMazeExplorerDemo.java
+++ lab11/lab11/graphs/TrivialMazeExplorerDemo.java
@@ -5,7 +5,7 @@ package lab11.graphs;
  */
 public class TrivialMazeExplorerDemo {
     public static void main(String[] args) {
-        Maze maze = new Maze("maze.config");
+        Maze maze = new Maze("lab11/graphs/maze.txt");
         TrivialMazeExplorer tme = new TrivialMazeExplorer(maze);
         tme.solve();
     }
