99baa93809d5325fbb96be97f628ea4ad7a195f6
Kevin Lowe <lowekevin123@gmail.com>
2018-04-07 13:33:58 -0700

    Update osm path to be consistent with library-sp18

--- proj3/src/main/java/GraphDBLauncher.java
+++ proj3/src/main/java/GraphDBLauncher.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
  * GraphDB in isolation from all the rest of the parts of this assignment.
  */
 public class GraphDBLauncher {
-    private static final String OSM_DB_PATH = "../library-sp18/data/berkeley-2018.osm.gz";
+    private static final String OSM_DB_PATH = "../library-sp18/data/berkeley-2018.osm.xml";
 
     public static void main(String[] args) {
         GraphDB g = new GraphDB(OSM_DB_PATH);
