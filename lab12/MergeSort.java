bd351f42c04daf133927b66fed055e9a7b2f0b25
nicole-rasquinha <nrasquinha@berkeley.edu>
2018-04-11 14:07:22 -0700

    Added lab 12 skeleton

---
+++ lab12/MergeSort.java
@@ -0,0 +1,66 @@
+import edu.princeton.cs.algs4.Queue;
+
+public class MergeSort {
+    /**
+     * Removes and returns the smallest item that is in q1 or q2.
+     *
+     * The method assumes that both q1 and q2 are in sorted order, with the smallest item first. At
+     * most one of q1 or q2 can be empty (but both cannot be empty).
+     *
+     * @param   q1  A Queue in sorted order from least to greatest.
+     * @param   q2  A Queue in sorted order from least to greatest.
+     * @return      The smallest item that is in q1 or q2.
+     */
+    private static <Item extends Comparable> Item getMin(
+            Queue<Item> q1, Queue<Item> q2) {
+        if (q1.isEmpty()) {
+            return q2.dequeue();
+        } else if (q2.isEmpty()) {
+            return q1.dequeue();
+        } else {
+            // Peek at the minimum item in each queue (which will be at the front, since the
+            // queues are sorted) to determine which is smaller.
+            Comparable q1Min = q1.peek();
+            Comparable q2Min = q2.peek();
+            if (q1Min.compareTo(q2Min) <= 0) {
+                // Make sure to call dequeue, so that the minimum item gets removed.
+                return q1.dequeue();
+            } else {
+                return q2.dequeue();
+            }
+        }
+    }
+
+    /** Returns a queue of queues that each contain one item from items. */
+    private static <Item extends Comparable> Queue<Queue<Item>>
+            makeSingleItemQueues(Queue<Item> items) {
+        // Your code here!
+        return null;
+    }
+
+    /**
+     * Returns a new queue that contains the items in q1 and q2 in sorted order.
+     *
+     * This method should take time linear in the total number of items in q1 and q2.  After
+     * running this method, q1 and q2 will be empty, and all of their items will be in the
+     * returned queue.
+     *
+     * @param   q1  A Queue in sorted order from least to greatest.
+     * @param   q2  A Queue in sorted order from least to greatest.
+     * @return      A Queue containing all of the q1 and q2 in sorted order, from least to
+     *              greatest.
+     *
+     */
+    private static <Item extends Comparable> Queue<Item> mergeSortedQueues(
+            Queue<Item> q1, Queue<Item> q2) {
+        // Your code here!
+        return null;
+    }
+
+    /** Returns a Queue that contains the given items sorted from least to greatest. */
+    public static <Item extends Comparable> Queue<Item> mergeSort(
+            Queue<Item> items) {
+        // Your code here!
+        return items;
+    }
+}
---
+++ lab12/QuickSort.java
@@ -0,0 +1,59 @@
+import edu.princeton.cs.algs4.Queue;
+
+public class QuickSort {
+    /**
+     * Returns a new queue that contains the given queues catenated together.
+     *
+     * The items in q2 will be catenated after all of the items in q1.
+     */
+    private static <Item extends Comparable> Queue<Item> catenate(Queue<Item> q1, Queue<Item> q2) {
+        Queue<Item> catenated = new Queue<Item>();
+        for (Item item : q1) {
+            catenated.enqueue(item);
+        }
+        for (Item item: q2) {
+            catenated.enqueue(item);
+        }
+        return catenated;
+    }
+
+    /** Returns a random item from the given queue. */
+    private static <Item extends Comparable> Item getRandomItem(Queue<Item> items) {
+        int pivotIndex = (int) (Math.random() * items.size());
+        Item pivot = null;
+        // Walk through the queue to find the item at the given index.
+        for (Item item : items) {
+            if (pivotIndex == 0) {
+                pivot = item;
+                break;
+            }
+            pivotIndex--;
+        }
+        return pivot;
+    }
+
+    /**
+     * Partitions the given unsorted queue by pivoting on the given item.
+     *
+     * @param unsorted  A Queue of unsorted items
+     * @param pivot     The item to pivot on
+     * @param less      An empty Queue. When the function completes, this queue will contain
+     *                  all of the items in unsorted that are less than the given pivot.
+     * @param equal     An empty Queue. When the function completes, this queue will contain
+     *                  all of the items in unsorted that are equal to the given pivot.
+     * @param greater   An empty Queue. When the function completes, this queue will contain
+     *                  all of the items in unsorted that are greater than the given pivot.
+     */
+    private static <Item extends Comparable> void partition(
+            Queue<Item> unsorted, Item pivot,
+            Queue<Item> less, Queue<Item> equal, Queue<Item> greater) {
+        // Your code here!
+    }
+
+    /** Returns a Queue that contains the given items sorted from least to greatest. */
+    public static <Item extends Comparable> Queue<Item> quickSort(
+            Queue<Item> items) {
+        // Your code here!
+        return items;
+    }
+}
