69b3e32bff83a7941b70051a9c06886bf77ebbf8
Eli Lipsitz <aegamesi@users.noreply.github.com>
2018-04-14 22:57:33 -0700

    proj3: make distance and bearing helper methods static

--- proj3/src/main/java/GraphDB.java
+++ proj3/src/main/java/GraphDB.java
@@ -90,7 +90,7 @@ public class GraphDB {
         return distance(lon(v), lat(v), lon(w), lat(w));
     }
 
-    double distance(double lonV, double latV, double lonW, double latW) {
+    static double distance(double lonV, double latV, double lonW, double latW) {
         double phi1 = Math.toRadians(latV);
         double phi2 = Math.toRadians(latW);
         double dphi = Math.toRadians(latW - latV);
@@ -117,7 +117,7 @@ public class GraphDB {
         return bearing(lon(v), lat(v), lon(w), lat(w));
     }
 
-    double bearing(double lonV, double latV, double lonW, double latW) {
+    static double bearing(double lonV, double latV, double lonW, double latW) {
         double phi1 = Math.toRadians(latV);
         double phi2 = Math.toRadians(latW);
         double lambda1 = Math.toRadians(lonV);
