7defd91002b72ff2f146c41f391cb5d51bda28f6
FlyingPig <1800012735@pku.edu.cn>
2020-09-09 22:12:17 +0800

    completed lab1: welcome to Java

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,8 +1,14 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Zhong Yinmin
  */
 public class LeapYear {
 
+    /** determin if the given year is a leap year
+    *   @param year to be analyzed
+    */
+    public static boolean isLeapYear(int year){
+        return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+    }
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
