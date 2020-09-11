26d0ca5822f5f2c3711c92e480e60b91bb4cd6b0
FlyingPig <1800012735@pku.edu.cn>
2020-09-11 23:26:59 +0800

    project1a: ArrayDeque.java -- tab -> space

--- proj1a/LinkedListDequeTest.java
+++ proj1a/LinkedListDequeTest.java
@@ -1,7 +1,7 @@
 /** Performs some basic linked list tests. */
 public class LinkedListDequeTest {
 
-	/* Utility method for printing out empty checks. */
+    /* Utility method for printing out empty checks. */
 	public static boolean checkEmpty(boolean expected, boolean actual) {
 		if (expected != actual) {
 			System.out.println("isEmpty() returned " + actual + ", but expected: " + expected);
