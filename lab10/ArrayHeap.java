658cb7018165339b4f669dac4811a592574a97ba
FlyingPig <1800012735@pku.edu.cn>
2020-09-22 11:49:37 +0800

    lab10: MinheapPQ--v4

--- lab10/ArrayHeap.java
+++ lab10/ArrayHeap.java
@@ -126,13 +126,15 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
      * Bubbles down the node currently at the given index.
      */
     private void sink(int index) {
+        if (!inBounds(index)) {
+            return;
+        }
+
         // Throws an exception if index is invalid. DON'T CHANGE THIS LINE.
         validateSinkSwimArg(index);
 
         /** TODO: Your code here. */
-        if (!inBounds(index)) {
-            return;
-        }
+
         int minChild = min(leftIndex(index), rightIndex(index));
         if (min(index, minChild) == minChild) {
             swap(index, minChild);
